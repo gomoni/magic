8f7a44025a494d543144c7adafff13c19da4c75e
Michal Vyskocil <michal.vyskocil@gmail.com>
2019-03-01 11:22:14 +0100

    P: crash on empty input
    S: define and return empty input error, use gofmt golint and go vet to
    fix nonGoisms in the code

--- magic.go
+++ magic.go
@@ -14,85 +14,91 @@ are not interesting.
 import "C"
 
 import (
-    "io"
-    "io/ioutil"
-    "fmt"
-    "unsafe"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"unsafe"
 )
 
-type Flag int
+type flag int
 
 const (
-    // print mime type; encoding
-    MAGIC_MIME Flag = C.MAGIC_MIME
+	// MagicMime - return mime type; encoding
+	MagicMime flag = C.MAGIC_MIME
 )
 
 var (
-// FIXME!!! why wrapped magic did not find the default DB??
-    emptyCString *C.char = C.CString("/usr/share/misc/magic")
+	// FIXME!!! why wrapped magic did not find the default DB??
+	emptyCString *C.char = C.CString("/usr/share/misc/magic")
+	errEmptyRequest         = errors.New("Empty request")
 )
 
-
+// Magic - wrapper for C magic_t of libmagic
 type Magic struct {
-    cookie C.magic_t
+	cookie C.magic_t
 }
 
 // magicError - return output of magic_error as Go error
 // panic if there was no error
 func magicError(cookie C.magic_t) error {
 
-    if cookie == nil {
-        return fmt.Errorf("Can't allocate magic cookie")
-    }
+	if cookie == nil {
+		return fmt.Errorf("Can't allocate magic cookie")
+	}
 
-    return fmt.Errorf(C.GoString(C.magic_error(cookie)))
+	return fmt.Errorf(C.GoString(C.magic_error(cookie)))
 }
 
 // New - allocate new magic cookie and set flags to MAGIC_MIME
 // returns an error in a case of failure
 func New() (*Magic, error) {
-    c := C.magic_open(C.int(0))
-    if c == nil {
-        return nil, magicError(c)
-    }
+	c := C.magic_open(C.int(0))
+	if c == nil {
+		return nil, magicError(c)
+	}
 
-    r := C.magic_setflags(c, C.int(MAGIC_MIME))
-    if r == -1 {
-        goto err
-    }
+	r := C.magic_setflags(c, C.int(MagicMime))
+	if r == -1 {
+		goto err
+	}
 
-    r = C.magic_load(c, emptyCString)
-    if r == -1 {
-        goto err
-    }
+	r = C.magic_load(c, emptyCString)
+	if r == -1 {
+		goto err
+	}
 
-    return &Magic{cookie: c}, nil
+	return &Magic{cookie: c}, nil
 
 err:
-    err := magicError(c)
-    C.magic_close(c)
-    return nil, err
+	err := magicError(c)
+	C.magic_close(c)
+	return nil, err
 }
 
 // Close - deallocate the cookie
 func (m *Magic) Close() {
-    C.magic_close(m.cookie)
+	C.magic_close(m.cookie)
 }
 
 // Mime - reads the content of a buffer and reports `mime; encoding`
 // or an error if that happen
 func (m *Magic) Mime(r io.Reader) (mime string, err error) {
-    var b []byte
-    b, err = ioutil.ReadAll(r)
-    if err != nil {
-        return
-    }
+	var b []byte
+	b, err = ioutil.ReadAll(r)
+	if err != nil {
+		return
+	}
+	if len(b) == 0 {
+		err = errEmptyRequest
+		return
+	}
 
-    cb := unsafe.Pointer(&b[0])
-    cmime := C.magic_buffer(m.cookie, cb, C.size_t(len(b)))
-    if cmime == nil {
-        err = magicError(m.cookie)
-        return
-    }
-    return C.GoString(cmime), nil
+	cb := unsafe.Pointer(&b[0])
+	cmime := C.magic_buffer(m.cookie, cb, C.size_t(len(b)))
+	if cmime == nil {
+		err = magicError(m.cookie)
+		return
+	}
+	return C.GoString(cmime), nil
 }
--- magic_test.go
+++ magic_test.go
@@ -1,40 +1,40 @@
 package magic
 
 import (
-    "testing"
-    "fmt"
-    "os"
+	"fmt"
+	"os"
+	"testing"
 )
 
 func TestMagic(t *testing.T) {
 
-    if _, err := os.Stat("/etc/passwd"); os.IsNotExist(err) {
-        t.Skip("/etc/passwd does not exists")
-    }
+	if _, err := os.Stat("/etc/passwd"); os.IsNotExist(err) {
+		t.Skip("/etc/passwd does not exists")
+	}
 
-    m, err := New()
-    if err != nil {
-        t.Fatal(err)
-    }
-    defer m.Close()
+	m, err := New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer m.Close()
 
-    f, err := os.Open("/etc/passwd")
-    if err != nil {
-        t.Fatal(err)
-    }
-    defer f.Close()
+	f, err := os.Open("/etc/passwd")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
 
-    mime, err := m.Mime(f)
-    if err != nil {
-        t.Fatal(err)
-    }
+	mime, err := m.Mime(f)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-    if mime != "text/plain; charset=us-ascii" {
-        t.Fatalf("Wrong mime type, expected 'text/plain; charset-us-ascii', got '%s'", mime)
-    }
+	if mime != "text/plain; charset=us-ascii" {
+		t.Fatalf("Wrong mime type, expected 'text/plain; charset-us-ascii', got '%s'", mime)
+	}
 
-    if testing.Verbose() {
-        fmt.Printf("magic /etc/passwd: %s\n", mime)
-    }
+	if testing.Verbose() {
+		fmt.Printf("magic /etc/passwd: %s\n", mime)
+	}
 
 }
