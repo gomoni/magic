c6fcd161b3476bf9fe1101a61eccf4419fc780ae
Michal Vyskocil <michal.vyskocil@gmail.com>
2019-03-08 10:22:27 +0100

    Problem: magic cookie is not thread safe
    Solution:
    1. rename `Magic` to `Cookie` (`magic.Cookie` is way better name for third
            party programs
    2. add `Pool` (`magic.Pool`) which is upper bounded and get/set cookies
       in a gorutine-safe way

--- magic.go
+++ magic.go
@@ -4,6 +4,9 @@ package magic
 Package magis is opinionated libmagic(1) gco wrapper returning mime type of a
 data. It does provide golang native interface and does not those, who
 are not interesting.
+
+Additionally implements bounded pool of magic cookies, so that library can be
+called from gorutines in a safe way.
 */
 
 /*
@@ -30,12 +33,12 @@ const (
 
 var (
 	// FIXME!!! why wrapped magic did not find the default DB??
-	emptyCString *C.char = C.CString("/usr/share/misc/magic")
+	emptyCString    *C.char = C.CString("/usr/share/misc/magic")
 	errEmptyRequest         = errors.New("Empty request")
 )
 
-// Magic - wrapper for C magic_t of libmagic
-type Magic struct {
+// Cookie - wrapper for C magic_t of libmagic
+type Cookie struct {
 	cookie C.magic_t
 }
 
@@ -52,7 +55,7 @@ func magicError(cookie C.magic_t) error {
 
 // New - allocate new magic cookie and set flags to MAGIC_MIME
 // returns an error in a case of failure
-func New() (*Magic, error) {
+func New() (*Cookie, error) {
 	c := C.magic_open(C.int(0))
 	if c == nil {
 		return nil, magicError(c)
@@ -68,7 +71,7 @@ func New() (*Magic, error) {
 		goto err
 	}
 
-	return &Magic{cookie: c}, nil
+	return &Cookie{cookie: c}, nil
 
 err:
 	err := magicError(c)
@@ -77,13 +80,14 @@ err:
 }
 
 // Close - deallocate the cookie
-func (m *Magic) Close() {
+func (m *Cookie) Close() {
 	C.magic_close(m.cookie)
 }
 
 // Mime - reads the content of a buffer and reports `mime; encoding`
 // or an error if that happen
-func (m *Magic) Mime(r io.Reader) (mime string, err error) {
+// Note: it is NOT gorutine safe
+func (m *Cookie) Mime(r io.Reader) (mime string, err error) {
 	var b []byte
 	b, err = ioutil.ReadAll(r)
 	if err != nil {
